6c8a49aaae628fc78f97303274062d3ed57b07ee
Romwil <romwil.pilapil@gmail.com>
2022-02-11 15:07:10 +0800

    GUI design upload, still in process

---
+++ cs_resume_creator/cs_resume_creator/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace cs_resume_creator
+{
+    partial class resume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstname_textbox = new System.Windows.Forms.TextBox();
+            this.firname_label = new System.Windows.Forms.Label();
+            this.mi_label = new System.Windows.Forms.Label();
+            this.mi_textbox = new System.Windows.Forms.TextBox();
+            this.lastname_textbox = new System.Windows.Forms.TextBox();
+            this.lastname_label = new System.Windows.Forms.Label();
+            this.mobile_label = new System.Windows.Forms.Label();
+            this.mobile_textbox = new System.Windows.Forms.TextBox();
+            this.email_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.address_richbox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstname_textbox
+            // 
+            this.firstname_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstname_textbox.Location = new System.Drawing.Point(103, 9);
+            this.firstname_textbox.Name = "firstname_textbox";
+            this.firstname_textbox.Size = new System.Drawing.Size(100, 22);
+            this.firstname_textbox.TabIndex = 0;
+            this.firstname_textbox.Text = "Romwil James";
+            // 
+            // firname_label
+            // 
+            this.firname_label.AutoSize = true;
+            this.firname_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firname_label.Location = new System.Drawing.Point(12, 9);
+            this.firname_label.Name = "firname_label";
+            this.firname_label.Size = new System.Drawing.Size(94, 20);
+            this.firname_label.TabIndex = 1;
+            this.firname_label.Text = "Firstname:";
+            // 
+            // mi_label
+            // 
+            this.mi_label.AutoSize = true;
+            this.mi_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mi_label.Location = new System.Drawing.Point(209, 9);
+            this.mi_label.Name = "mi_label";
+            this.mi_label.Size = new System.Drawing.Size(115, 20);
+            this.mi_label.TabIndex = 2;
+            this.mi_label.Text = "Middle Initial:";
+            // 
+            // mi_textbox
+            // 
+            this.mi_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mi_textbox.Location = new System.Drawing.Point(321, 9);
+            this.mi_textbox.Name = "mi_textbox";
+            this.mi_textbox.Size = new System.Drawing.Size(27, 22);
+            this.mi_textbox.TabIndex = 3;
+            this.mi_textbox.Text = "D.";
+            // 
+            // lastname_textbox
+            // 
+            this.lastname_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastname_textbox.Location = new System.Drawing.Point(445, 9);
+            this.lastname_textbox.Name = "lastname_textbox";
+            this.lastname_textbox.Size = new System.Drawing.Size(83, 22);
+            this.lastname_textbox.TabIndex = 4;
+            this.lastname_textbox.Text = "Pilapil";
+            // 
+            // lastname_label
+            // 
+            this.lastname_label.AutoSize = true;
+            this.lastname_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastname_label.Location = new System.Drawing.Point(354, 9);
+            this.lastname_label.Name = "lastname_label";
+            this.lastname_label.Size = new System.Drawing.Size(93, 20);
+            this.lastname_label.TabIndex = 5;
+            this.lastname_label.Text = "Lastname:";
+            // 
+            // mobile_label
+            // 
+            this.mobile_label.AutoSize = true;
+            this.mobile_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobile_label.Location = new System.Drawing.Point(12, 37);
+            this.mobile_label.Name = "mobile_label";
+            this.mobile_label.Size = new System.Drawing.Size(98, 20);
+            this.mobile_label.TabIndex = 6;
+            this.mobile_label.Text = "Mobile No: ";
+            // 
+            // mobile_textbox
+            // 
+            this.mobile_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobile_textbox.Location = new System.Drawing.Point(103, 37);
+            this.mobile_textbox.Name = "mobile_textbox";
+            this.mobile_textbox.Size = new System.Drawing.Size(100, 22);
+            this.mobile_textbox.TabIndex = 7;
+            this.mobile_textbox.Text = "09874561230";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.Location = new System.Drawing.Point(209, 37);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(58, 20);
+            this.email_label.TabIndex = 2;
+            this.email_label.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(264, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "romwil.pilapil@gmail.com";
+            // 
+            // address_richbox
+            // 
+            this.address_richbox.Location = new System.Drawing.Point(16, 108);
+            this.address_richbox.Name = "address_richbox";
+            this.address_richbox.Size = new System.Drawing.Size(261, 85);
+            this.address_richbox.TabIndex = 9;
+            this.address_richbox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Address:";
+            // 
+            // resume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.address_richbox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mobile_textbox);
+            this.Controls.Add(this.mobile_label);
+            this.Controls.Add(this.lastname_textbox);
+            this.Controls.Add(this.lastname_label);
+            this.Controls.Add(this.mi_textbox);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.mi_label);
+            this.Controls.Add(this.firstname_textbox);
+            this.Controls.Add(this.firname_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "resume";
+            this.Text = "PDF resume creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstname_textbox;
+        private System.Windows.Forms.Label firname_label;
+        private System.Windows.Forms.Label mi_label;
+        private System.Windows.Forms.TextBox mi_textbox;
+        private System.Windows.Forms.TextBox lastname_textbox;
+        private System.Windows.Forms.Label lastname_label;
+        private System.Windows.Forms.Label mobile_label;
+        private System.Windows.Forms.TextBox mobile_textbox;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox address_richbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
